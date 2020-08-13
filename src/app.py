eae8c04eec7f2dc6a0b6b701e2e5f8df259b7dd9
Christopher Gidney <chris.gidney@live.co.uk>
2020-08-13 23:52:41 +0100

    Removed unused route

--- src/app.py
+++ src/app.py
@@ -3,17 +3,12 @@ import ipretriever
 
 app = Flask(__name__)
 
+
 @app.route('/')
 def index():
     networkIp = ipretriever.getIp()
     requestIp = request.remote_addr
     return render_template('home.html', networkIp=networkIp, requestIp=requestIp)
-
-
-@app.route('/hello/')
-@app.route('/hello/<name>')
-def hello(name=None):
-    return render_template('hello.html', name=name)
 
 
 if __name__ == '__main__':
