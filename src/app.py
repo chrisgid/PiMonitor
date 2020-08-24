bf8653b70b52e46dfd8afce94a9d072f1c12bcda
Christopher Gidney <chris.gidney@live.co.uk>
2020-08-24 23:02:03 +0100

    Added test speedtest route

--- src/app.py
+++ src/app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request, jsonify
 from result import get_results
+import speedtest
 import ipretriever
 
 app = Flask(__name__)
@@ -11,10 +12,17 @@ def index():
     requestIp = request.remote_addr
     return render_template('home.html', networkIp=networkIp, requestIp=requestIp)
 
+
 @app.route('/api/result')
 def result():
     results = get_results(test=True)
     return jsonify(results)
+
+
+@app.route('/api/speedtest')
+def run_speedtest():
+    '''Run a speedtest and output the result. For testing purposes'''
+    return jsonify(speedtest.run_speedtest().speed_dict)
 
 
 if __name__ == '__main__':
